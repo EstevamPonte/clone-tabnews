19b80d1efbcef2a8cbb6cb462bd2aec76dcdb840
Estevam Ponte <tecoponte@gmail.com>
2025-02-18 21:16:24 -0300

    Ninishing status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,35 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "sao acima da media" });
+  const updatedAt = new Date().toISOString();
+
+  const dataBaseVersionResult = await database.query("SHOW server_version;");
+  const dataBaseVersion = dataBaseVersionResult.rows[0].server_version;
+
+  const dataBaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const dataBaseMaxConnectionsValue =
+    dataBaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const dataBaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const dataBaseOpenedConnectionsValue =
+    dataBaseOpenedConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dataBaseVersion,
+        max_connections: parseInt(dataBaseMaxConnectionsValue),
+        opened_connections: dataBaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
