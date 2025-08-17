d08eaf2ef2400902bbf83326f1992a9db1eccb26
Estevam Alcântara Ponte <tecoponte@gmail.com>
2025-08-17 21:35:30 +0000

    feat: implement api/v1/user endpoint with session renewal

--- infra/controller.js
+++ infra/controller.js
@@ -6,6 +6,9 @@ import {
   UnauthorizedError,
 } from "infra/errors";
 
+import * as cookie from "cookie";
+import session from "models/session";
+
 function onNoMatchHandler(request, response) {
   const publicErrorObject = new MethodNotAllowedError();
   response.status(publicErrorObject.statusCode).json(publicErrorObject);
@@ -27,11 +30,23 @@ function onErrorHandler(error, request, response) {
   response.status(publicErrorObject.statusCode).json(publicErrorObject);
 }
 
+function setSessionCookie(sessionToken, response) {
+  const setCookie = cookie.serialize("session_id", sessionToken, {
+    path: "/",
+    maxAge: session.EXPIRATIONS_IN_MILLISECONDS / 1000,
+    secure: process.env.NODE_ENV === "production",
+    httpOnly: true,
+  });
+
+  response.setHeader("Set-Cookie", setCookie);
+}
+
 const controller = {
   errorHandlers: {
     onNoMatch: onNoMatchHandler,
     onError: onErrorHandler,
   },
+  setSessionCookie,
 };
 
 export default controller;
